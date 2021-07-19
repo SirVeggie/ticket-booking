4b737689f087270a10bc7c983a07944bf4711e10
Veikka <32365239+SirVeggie@users.noreply.github.com>
2021-07-20 01:24:45 +0300

    Admin add showtime view

--- frontend/src/views/admin/AdminAddShowtime.tsx
+++ frontend/src/views/admin/AdminAddShowtime.tsx
@@ -1,11 +1,92 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Button, Divider, Dropdown, Input } from 'semantic-ui-react';
+import Card from '../../components/Card';
+import { Showtime } from '../../datatypes';
+import { setData } from '../../reducers/dataReducer';
+import { StateType } from '../../store';
+import database from '../../tools/database';
+import { mapShowtimeCard } from '../Showtimes';
 
-function AdminAddShowtime() {
+const defaultDate = new Date(2000, 0, 1, 0, 0);
+
+export default function AdminAddShowtime() {
+  const dispatch = useDispatch();
+  const shows = useSelector((state: StateType) => state.data.shows);
+  const [showtime, setShowtime] = useState({ ...new Showtime(), date: defaultDate });
+  const [dateError, setDateError] = useState(false);
+  const show = shows.find(x => x.id === showtime.showid);
+  
+  const parseDate = (value: string) => {
+    let data = value.split(' ');
+    if (data.length !== 2) {
+      setDateError(true);
+      return defaultDate;
+    }
+    
+    setDateError(false);
+    if (data[1].includes(':'))
+      data = data.reverse();
+    const time = data[0].split(':').map(x => Number(x));
+    const date = data[1].split(/[./-]/).map(x => Number(x));
+    return new Date(date[2], date[1] - 1, date[0], time[0], time[1]);
+  };
+  
+  const onSave = async () => {
+    try {
+      await database.showtimes.add(showtime);
+    } catch {
+      return;
+    }
+    
+    dispatch(setData(await database.getPacket()));
+    setShowtime({ ...new Showtime(), date: defaultDate });
+  };
+  
   return (
-    <div>
+    <div className='ui container'>
       <h1>Add showtime</h1>
+      <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+        <LabeledDropdown label='Show' update={value => setShowtime({ ...showtime, showid: value })} />
+        <InputField label='Location' update={value => setShowtime({ ...showtime, location: value })} />
+        <InputField label='Date' error={dateError} update={value => setShowtime({ ...showtime, date: parseDate(value) })} />
+        <InputField type='number' label='Max seats' update={value => setShowtime({ ...showtime, maxSeats: Number(value) })} />
+        <InputField type='number' label='Normal price' update={value => setShowtime({ ...showtime, prices: { ...showtime.prices, normal: Number(value) } })} />
+        <InputField type='number' label='Discount price' update={value => setShowtime({ ...showtime, prices: { ...showtime.prices, discount: Number(value) } })} />
+        <InputField type='number' label='Family price' update={value => setShowtime({ ...showtime, prices: { ...showtime.prices, family: Number(value) } })} />
+      </div>
+      <Button onClick={onSave}>Save</Button>
+      <Divider />
+      <h2>Card preview</h2>
+      <Card data={mapShowtimeCard(show?.name ?? '(no show)', showtime)} />
     </div>
   );
 }
 
-export default AdminAddShowtime;
+function InputField({ label, update, type, error }: { label: string, update: (value: string) => void, type?: string, error?: boolean }) {
+  const [value, setValue] = useState('');
+
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(event.target.value);
+    update(event.target.value);
+  };
+  
+  return (
+    <div>
+      <label>{label}</label><br />
+      <Input type={type} error={error} size='small' value={value} onChange={onChange} style={{ margin: '0 10px 10px 0' }} />
+    </div>
+  );
+}
+
+function LabeledDropdown({ label, update }: { label: string, update: (value: any) => void; }) {
+  const shows = useSelector((state: StateType) => state.data.shows);
+  const options = shows.map(x => ({ key: x.id, value: x.id, text: x.name }));
+  
+  return (
+    <div>
+      <label>{label}</label><br />
+      <Dropdown search selection compact style={{ width: 177, margin: '0 10px 10px 0' }} options={options} onChange={(event, dropdata) => update(dropdata.value)} />
+    </div>
+  );
+}
