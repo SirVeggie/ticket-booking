9862d5d76a4afe7fab38ab1ac244c130a59d9cd1
Veikka <32365239+SirVeggie@users.noreply.github.com>
2021-07-20 01:23:13 +0300

    Simpler centering component

---
+++ frontend/src/components/Center.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Center({ className, style, children }: { className?: string, style?: React.CSSProperties, children: any }) {
+  return (
+    <div className={className} style={{ ...center, ...style }}>
+      {children}
+    </div>
+  );
+}
+
+const center: React.CSSProperties = {
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center'
+};
+
+export default Center;
