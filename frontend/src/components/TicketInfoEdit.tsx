ad523a055ba59b9dcc659f84f50e23900ce53c5b
Veikka <32365239+SirVeggie@users.noreply.github.com>
2022-07-25 16:41:23 +0300

    fix useEffect leak #pull

--- frontend/src/components/TicketInfoEdit.tsx
+++ frontend/src/components/TicketInfoEdit.tsx
@@ -5,11 +5,12 @@ import { Button, Message } from 'semantic-ui-react';
 import { Seats, sumSeats, sumTickets, Ticket } from 'shared';
 import { StateType } from '../store';
 import database from '../tools/database';
+import { RePromise } from '../tools/RePromise';
 import { ConfirmationModal } from './ConfirmationModal';
 import LabelDropdown from './LabelDropdown';
 import Toggle from './Toggle';
 
-export default function TicketInfoEdit({ ticket, update }: { ticket: Ticket, update?: (x: Ticket) => void }) {
+export default function TicketInfoEdit({ ticket, update }: { ticket: Ticket, update?: (x: Ticket) => void; }) {
   const history = useHistory();
   const admin = useSelector((state: StateType) => state.admin.status);
   const [available, setAvailable] = useState(0);
@@ -18,57 +19,63 @@ export default function TicketInfoEdit({ ticket, update }: { ticket: Ticket, upd
   const [loading, setLoading] = useState(false);
   const [numbers, setNumbers] = useState([0]);
   const [modal, setModal] = useState(false);
-  
+
   useEffect(() => {
     if (!ticket.showtimeid)
       return;
     setSeats(ticket.seats);
-    database.showtimes.getAvailableSeats(ticket.showtimeid)
-      .then(x => {
-        setAvailable(x);
-        setNumbers(Array(Math.max(sumSeats(ticket.seats), admin ? 200 : 50) + 1).fill(0).map((x, i) => i));
-      }).catch(() => setNumbers([0]));
+    const p = RePromise(database.showtimes.getAvailableSeats(ticket.showtimeid));
+    p.then(x => {
+      setAvailable(x);
+      setNumbers(Array(Math.max(sumSeats(ticket.seats), admin ? 200 : 50) + 1).fill(0).map((x, i) => i));
+    }).catch(e => {
+      if (e.canceled)
+        return;
+      setNumbers([0]);
+    });
+
+    return () => p.reject({ canceled: true });
   }, [ticket]);
-  
+
   const updateSeats = (data: Seats) => {
     setSeats(data);
-    
+
     if (sumSeats(data) > available + sumSeats(ticket.seats)) {
       setError(`Ei riittävästi paikkoja vapaana. Paikkamäärä ylittyy ${sumSeats(data) - (sumSeats(ticket.seats) + available)} paikalla.`);
     } else {
       setError('');
     }
   };
-  
+
   const onSave = () => {
     if (sumTickets(seats) === 0) {
       setError('Valitse ainakin yksi lippu');
       return;
     }
-    
+
     if (ticket.seats.normal === seats.normal && ticket.seats.discount === seats.discount && ticket.seats.family === seats.family) {
       setError('Varausta ei ole muutettu');
       return;
     }
-    
+
     if (!admin && sumSeats(seats) > available + sumSeats(ticket.seats)) {
       setError('Valitut liput ylittävät vapaidet paikkojen määrän. Vähennä lippujen määrää.');
       return;
     }
-    
+
     setLoading(true);
     setModal(true);
   };
-  
+
   const onConfirm = async (confirm: boolean) => {
     await onConfirmAction(confirm);
     setLoading(false);
   };
-  
+
   const onConfirmAction = async (confirm: boolean) => {
     setModal(false);
     if (!confirm) return;
-    
+
     try {
       await database.tickets.updateSeats(ticket.id, seats);
       update?.call(null, { ...ticket, seats: seats });
@@ -77,7 +84,7 @@ export default function TicketInfoEdit({ ticket, update }: { ticket: Ticket, upd
       setError('Lipun päivitys epäonnistui');
     }
   };
-  
+
   return (
     <div>
       <ConfirmationModal
@@ -90,19 +97,19 @@ export default function TicketInfoEdit({ ticket, update }: { ticket: Ticket, upd
         Alennusliput: {ticket.seats.discount} {'->'} {seats.discount}<br />
         Perheliput: {ticket.seats.family} {'->'} {seats.family}<br />
       </ConfirmationModal>
-      
+
       <h2>Vaihda varattujen lippujen määrää</h2>
-      
+
       <Toggle enabled={!!error}>
         <Message icon='warning sign' warning size='small' header={error} />
       </Toggle>
-      
+
       <div style={{ display: 'flex', flexWrap: 'wrap' }}>
         <LabelDropdown label='Perusliput' value={x => x === seats.normal} items={numbers} mapName={item => String(item)} update={x => updateSeats({ ...seats, normal: x })} />
         <LabelDropdown label='Alennusliput' value={x => x === seats.discount} items={numbers} mapName={item => String(item)} update={x => updateSeats({ ...seats, discount: x })} />
         <LabelDropdown label='Perheliput' value={x => x === seats.family} items={numbers} mapName={item => String(item)} update={x => updateSeats({ ...seats, family: x })} />
       </div>
-      
+
       <Button basic loading={loading} onClick={onSave}>Confirm</Button>
       <Button basic onClick={() => history.push(`/ticket/${ticket.id}`)}>Back</Button>
     </div>
