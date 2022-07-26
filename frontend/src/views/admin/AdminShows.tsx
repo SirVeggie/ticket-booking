f2238c9447a6e676160a5b170a5b7743de122020
Veikka <32365239+SirVeggie@users.noreply.github.com>
2022-07-26 17:10:07 +0300

    delete shows

--- frontend/src/views/admin/AdminShows.tsx
+++ frontend/src/views/admin/AdminShows.tsx
@@ -3,17 +3,23 @@ import { useSelector } from 'react-redux';
 import Card from '../../components/Card';
 import CardButtons from '../../components/CardButtons';
 import { StateType } from '../../store';
+import database from '../../tools/database';
 import { showMapper } from '../Homepage';
 
 export default function AdminShows() {
   const { shows, showtimes, misc } = useSelector((state: StateType) => state.data);
   const cards = showMapper(shows, showtimes, misc);
+  const deleteShow = (title: string) => {
+    const id = shows.find(x => x.name === title)?.id;
+    if (id)
+      database.shows.delete(id);
+  };
 
   return (
     <div className='ui container'>
       {!cards ? 'No shows available' : cards.map((card, i) => (
         <Card style={{ marginBottom: 10 }} key={i} data={card} onClick={card.action ?? (() => console.log('Clicked ' + i))}>
-          <CardButtons aEdit={() => console.log('Edit pressed')} aHide={() => console.log('Hide pressed')} aDelete={() => console.log('Delete pressed')} />
+          <CardButtons aEdit={() => console.log('Edit pressed')} aHide={() => console.log('Hide pressed')} aDelete={() => deleteShow(card.title)} />
         </Card>
       ))}
     </div>
